8a2a65f5d46aece3f9e4bb473ada0475ae6e3396
naufal453 <105023440+naufal453@users.noreply.github.com>
2025-04-12 11:31:25 +0700

    Create 404.blade.php

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>404 Not Found</title>
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    </head>
+
+    <body>
+        <div class="container">
+            <h1>404 Not Found</h1>
+            <h2>HAYOOO MAU BUKA APAAN</h2>
+            <p>Sorry, the page you are looking for could not be found.</p>
+            <a href="{{ url('/') }}">Return to Home</a>
+        </div>
+    </body>
+
+</html>
