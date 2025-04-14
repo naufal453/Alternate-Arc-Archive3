77d96fb9121cafc9461b3bd97b920a9815cfb429
Naufal453 <alamsyahnaufal453@gmail.com>
2025-04-14 14:20:16 +0700

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,74 +6,83 @@
             margin-top: 10px;
             margin-bottom: 10px;
         }
+
+        .login-image {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            flex-direction: column;
+            color: #fff;
+            font-size: 24px;
+            font-weight: bold;
+            text-align: center;
+        }
+
+        .brand-logo {
+            max-width: 100%;
+            height: auto;
+            margin-bottom: 20px;
+        }
     </style>
-    <section class="ftco-section">
-        <div class="container">
-            <div class="row justify-content-center">
-                <div class="col-md-6 text-center mb-5">
-                    <h2 class="heading-section">Login</h2>
-                </div>
-            </div>
-            <div class="row justify-content-center">
-                <div class="col-md-7 col-lg-5">
-                    <div class="wrap">
-                        <div class="img" style="background-image: url('{{ asset('images/bg-1.jpg') }}');"></div>
-                        <div class="login-wrap p-4 p-md-5">
-                            <div class="d-flex">
-                                <div class="w-100">
-                                    <p class="social-media d-flex justify-content-end">
-                                        <a href="#"
-                                            class="social-icon d-flex align-items-center justify-content-center"><span
-                                                class="fa fa-facebook"></span></a>
-                                        <a href="#"
-                                            class="social-icon d-flex align-items-center justify-content-center"><span
-                                                class="fa fa-twitter"></span></a>
-                                    </p>
+
+    <body style="background-color: #eee">
+        <div class="container d-flex justify-content-center align-items-center min-vh-100">
+            <div class="row justify-content-center w-100">
+                <div class="col-md-8 col-lg-6 col-xl-5">
+                    <div class="card rounded-3 text-black w-100">
+                        <div class="row g-0 justify-content-center">
+                            <!-- Login Form -->
+                            <div class="col-lg-12">
+                                <div class="card-body p-md-5 mx-md-3 text-center">
+
+                                    <form method="post" action="{{ route('login.perform') }}">
+                                        @csrf
+                                        <h3>Login</h3>
+
+                                        <div class="form-outline mb-4">
+                                            <br>
+                                            <input type="text" id="username" class="form-control" name="username"
+                                                value="{{ old('username') }}" placeholder="Username" required autofocus>
+
+                                            @if ($errors->has('username'))
+                                                <span class="text-danger text-left">{{ $errors->first('username') }}</span>
+                                            @endif
+                                        </div>
+
+                                        <div class="form-outline mb-4">
+                                            <br>
+                                            <input id="password-field" type="password" class="form-control" name="password"
+                                                placeholder="Password" required>
+
+                                            <span toggle="#password-field"
+                                                class="fa fa-fw fa-eye field-icon toggle-password"></span>
+                                            @if ($errors->has('password'))
+                                                <span class="text-danger text-left">{{ $errors->first('password') }}</span>
+                                            @endif
+                                        </div>
+
+                                        <div class="text-center pt-1 mb-5 pb-1" style="list-style: none;">
+                                            <li>
+                                                <button type="submit" class="btn btn-primary btn-block fa-lg mb-3">
+                                                    Login
+                                                </button>
+                                            </li>
+                                            <a class="text-muted" href="#">Forgot password?</a>
+                                        </div>
+
+                                        <div class="d-flex align-items-center justify-content-center pb-4">
+                                            <p class="mb-0 me-2">Don't have an account?</p>
+                                            <a href="{{ route('register.perform') }}" class="btn btn-outline-danger">Create
+                                                new</a>
+                                        </div>
+                                    </form>
                                 </div>
                             </div>
-                            <form method="post" action="{{ route('login.perform') }}" class="signin-form">
-                                @csrf
-                                <div class="form-group mt-3">
-                                    <input type="text" class="form-control" name="username" value="{{ old('username') }}"
-                                        placeholder="Username" required="required" autofocus>
-
-                                    @if ($errors->has('username'))
-                                        <span class="text-danger text-left">{{ $errors->first('username') }}</span>
-                                    @endif
-                                </div>
-                                <div class="form-group">
-                                    <input id="password-field" type="password" class="form-control" name="password"
-                                        value="{{ old('password') }}" placeholder="Password" required="required">
-
-                                    <span toggle="#password-field"
-                                        class="fa fa-fw fa-eye field-icon toggle-password"></span>
-                                    @if ($errors->has('password'))
-                                        <span class="text-danger text-left">{{ $errors->first('password') }}</span>
-                                    @endif
-                                </div>
-                                <div class="form-group">
-                                    <button type="submit" class="form-control btn btn-primary rounded submit px-3">Sign
-                                        In</button>
-                                </div>
-                                <div class="form-group d-md-flex">
-                                    <div class="w-50 text-left">
-                                        <label class="checkbox-wrap checkbox-primary mb-0">Remember Me
-                                            <input type="checkbox" name="remember" checked>
-                                            <span class="checkmark"></span>
-                                        </label>
-                                    </div>
-                                    <div class="w-50 text-md-right">
-                                        <a href="#">Forgot Password</a>
-                                    </div>
-                                </div>
-                            </form>
-                            <p class="text-center">Not a member? <a href="{{ route('register.perform') }}">Sign Up</a></p>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-    </section>
+    </body>
 @endsection
 
 @section('scripts')
