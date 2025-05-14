bed9a437843a6ef4a0dfb0794134fad57aaae58a
naufal453 <105023440+naufal453@users.noreply.github.com>
2025-05-14 09:34:29 +0700

    update

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,94 +1,69 @@
 @extends('layouts.auth-master')
 
 @section('content')
-    <style>
-        input {
-            margin-top: 10px;
-            margin-bottom: 10px;
-        }
-
-        .login-image {
-            display: flex;
-            align-items: center;
-            justify-content: center;
-            flex-direction: column;
-            color: #fff;
-            font-size: 24px;
-            font-weight: bold;
-            text-align: center;
-        }
-
-        .brand-logo {
-            max-width: 100%;
-            height: auto;
-            margin-bottom: 20px;
-        }
-    </style>
-
-    <body style="background-color: #eee">
-        <div class="container d-flex justify-content-center align-items-center min-vh-100">
-            <div class="row justify-content-center w-100">
-                <div class="col-md-8 col-lg-6 col-xl-5">
-                    <div class="card rounded-3 text-black w-100">
-                        <div class="row g-0 justify-content-center">
-                            <!-- Login Form -->
-                            <div class="col-lg-12">
-                                <div class="card-body p-md-5 mx-md-3 text-center">
-
-                                    <form method="post" action="{{ route('login.perform') }}">
-                                        @csrf
-                                        <h3>Login</h3>
-
-                                        <div class="form-outline mb-4">
-                                            <br>
-                                            <input type="text" id="username" class="form-control" name="username"
-                                                value="{{ old('username') }}" placeholder="Username" required autofocus>
-
-                                            @if ($errors->has('username'))
-                                                <span class="text-danger text-left">{{ $errors->first('username') }}</span>
-                                            @endif
-                                        </div>
-
-                                        <div class="form-outline mb-4">
-                                            <br>
-                                            <input id="password-field" type="password" class="form-control" name="password"
-                                                placeholder="Password" required>
-
-                                            <span toggle="#password-field"
-                                                class="fa fa-fw fa-eye field-icon toggle-password"></span>
-                                            @if ($errors->has('password'))
-                                                <span class="text-danger text-left">{{ $errors->first('password') }}</span>
-                                            @endif
-                                        </div>
-
-                                        <div class="text-center pt-1 mb-5 pb-1" style="list-style: none;">
-                                            <li>
-                                                <button type="submit" class="btn btn-primary btn-block fa-lg mb-3">
-                                                    Login
-                                                </button>
-                                            </li>
-                                            <a class="text-muted" href="#">Forgot password?</a>
-                                        </div>
-
-                                        <div class="d-flex align-items-center justify-content-center pb-4">
-                                            <p class="mb-0 me-2">Don't have an account?</p>
-                                            <a href="{{ route('register.perform') }}" class="btn btn-outline-danger">Create
-                                                new</a>
-                                        </div>
-                                    </form>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+<body>
+    <link rel="stylesheet" href="{{ asset('css/login.css') }}">
+<title>Login Page</title>
+    <div class="auth-container">
+        <!-- Background Illustrations -->
+        <div class="illustration-bg">
+            <div class="illustration-item dots-1"></div>
+            <div class="illustration-item dots-2"></div>
+            <div class="illustration-item line-1"></div>
+            <div class="illustration-item line-2"></div>
+            <div class="illustration-item wave wave-1"></div>
+            <div class="illustration-item wave wave-2"></div>
+        </div>
+        
+        <div class="login-card shadow-sm">
+            <div class="auth-header">
+                <h2 class="auth-title">Login</h2>
+                <p class="auth-subtitle">Hey, Enter your details to get sign in to your account</p>
             </div>
-    </body>
+            
+            <form method="post" action="{{ route('login.perform') }}">
+                @csrf
+                
+                <div class="form-group">
+                    <input type="text" id="username" class="form-control" name="username" 
+                        value="{{ old('username') }}" placeholder="Enter Email / Phone No" required autofocus>
+                    @if ($errors->has('username'))
+                        <span class="text-danger text-left">{{ $errors->first('username') }}</span>
+                    @endif
+                </div>
+                
+                <div class="form-group password-field-container">
+                    <input id="password-field" type="password" class="form-control" name="password" 
+                        placeholder="Passcode" required>
+                    <span toggle="#password-field" class="fa fa-fw fa-eye field-icon toggle-password"></span>
+                    @if ($errors->has('password'))
+                        <span class="text-danger text-left">{{ $errors->first('password') }}</span>
+                    @endif
+                </div>
+                
+
+                <div class="form-group">
+                    <button type="submit" class="btn btn-primary w-100">
+                        Sign in
+                    </button>
+                </div>
+            </form>
+            
+
+            
+            <div class="auth-footer">
+                Don't have an account? <a href="{{ route('register.perform') }}">Request Now</a>
+            </div>
+        </div>
+    </div>
+</body>
+    
 @endsection
 
 @section('scripts')
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js"></script>
     <script>
         $(document).ready(function() {
             $('.toggle-password').click(function() {
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,87 +1,85 @@
-@extends('layouts.auth-master')
-<style>
-    input {
-        margin-top: 10px;
-        margin-bottom: 10px;
-    }
-</style>
-@section('content')
-    <section class="ftco-section">
-        <div class="container">
-            <div class="row justify-content-center">
-                <div class="col-md-6 text-center mb-5">
-                    <h2 class="heading-section">Register</h2>
-                </div>
-            </div>
-            <div class="row justify-content-center">
-                <div class="col-md-7 col-lg-5">
-                    <div class="wrap">
-                        <div class="img" style="background-image: url('{{ asset('images/bg-1.jpg') }}');"></div>
-                        <div class="login-wrap p-4 p-md-5">
-                            <div class="d-flex">
-                                <div class="w-100">
-                                    <p class="social-media d-flex justify-content-end">
-                                        <a href="#"
-                                            class="social-icon d-flex align-items-center justify-content-center"><span
-                                                class="fa fa-facebook"></span></a>
-                                        <a href="#"
-                                            class="social-icon d-flex align-items-center justify-content-center"><span
-                                                class="fa fa-twitter"></span></a>
-                                    </p>
-                                </div>
-                            </div>
-                            <form method="post" action="{{ route('register.perform') }}" class="signin-form">
-                                @csrf
-                                <div class="form-group mt-3">
-                                    <input type="text" class="form-control" name="username" value="{{ old('username') }}"
-                                        placeholder="Username" required="required" autofocus>
 
-                                    @if ($errors->has('username'))
-                                        <span class="text-danger text-left">{{ $errors->first('username') }}</span>
-                                    @endif
-                                </div>
-                                <div class="form-group mt-3">
-                                    <input type="email" class="form-control" name="email" value="{{ old('email') }}"
-                                        placeholder="Email" required="required">
-                                    @if ($errors->has('email'))
-                                        <span class="text-danger text-left">{{ $errors->first('email') }}</span>
-                                    @endif
-                                </div>
-                                <div class="form-group mt-3">
-                                    <input type="password" class="form-control" name="password"
-                                        value="{{ old('password') }}" placeholder="Password" required="required">
-                                    @if ($errors->has('password'))
-                                        <span class="text-danger text-left">{{ $errors->first('password') }}</span>
-                                    @endif
-                                </div>
-                                <div class="form-group mt-3">
-                                    <input type="password" class="form-control" name="password_confirmation"
-                                        value="{{ old('password_confirmation') }}" placeholder="Confirm Password"
-                                        required="required">
-                                    @if ($errors->has('password_confirmation'))
-                                        <span
-                                            class="text-danger text-left">{{ $errors->first('password_confirmation') }}</span>
-                                    @endif
-                                </div>
-                                <br>
-                                <div class="form-group">
-                                    <button type="submit"
-                                        class="form-control btn btn-primary rounded submit px-3">Register</button>
-                                </div>
-                            </form>
-                            <p class="text-center">Already a member? <a href="{{ route('login.perform') }}">Sign In</a></p>
-                        </div>
-                    </div>
+
+@extends('layouts.auth-master')
+
+@section('content')
+<body>
+    <link rel="stylesheet" href="{{ asset('css/register.css') }}">
+ <title>Registration Page</title>
+    <div class="auth-container">
+        <!-- Background Illustrations -->
+        <div class="illustration-bg">
+            <div class="illustration-item dots-1"></div>
+            <div class="illustration-item dots-2"></div>
+            <div class="illustration-item line-1"></div>
+            <div class="illustration-item line-2"></div>
+            <div class="illustration-item wave wave-1"></div>
+            <div class="illustration-item wave wave-2"></div>
+        </div>
+        
+        <div class="login-card shadow-sm">
+            <div class="auth-header">
+                <h2 class="auth-title">Create Account</h2>
+                <p class="auth-subtitle">Please fill in your details to register</p>
+            </div>
+            
+            <form method="post" action="{{ route('register.perform') }}">
+                @csrf
+                
+                <div class="form-group">
+                    <input type="text" class="form-control" name="username" value="{{ old('username') }}"
+                        placeholder="Username" required autofocus>
+                    @if ($errors->has('username'))
+                        <span class="text-danger text-left">{{ $errors->first('username') }}</span>
+                    @endif
                 </div>
+                
+                <div class="form-group">
+                    <input type="email" class="form-control" name="email" value="{{ old('email') }}"
+                        placeholder="Email Address" required>
+                    @if ($errors->has('email'))
+                        <span class="text-danger text-left">{{ $errors->first('email') }}</span>
+                    @endif
+                </div>
+                
+                <div class="form-group password-field-container">
+                    <input id="password-field" type="password" class="form-control" name="password"
+                        value="{{ old('password') }}" placeholder="Password" required>
+                    <span toggle="#password-field" class="fa fa-fw fa-eye field-icon toggle-password"></span>
+                    @if ($errors->has('password'))
+                        <span class="text-danger text-left">{{ $errors->first('password') }}</span>
+                    @endif
+                </div>
+                
+                <div class="form-group password-field-container">
+                    <input id="password-confirm-field" type="password" class="form-control" name="password_confirmation"
+                        value="{{ old('password_confirmation') }}" placeholder="Confirm Password" required>
+                    <span toggle="#password-confirm-field" class="fa fa-fw fa-eye field-icon toggle-password"></span>
+                    @if ($errors->has('password_confirmation'))
+                        <span class="text-danger text-left">{{ $errors->first('password_confirmation') }}</span>
+                    @endif
+                </div>
+                
+                <div class="form-group">
+                    <button type="submit" class="btn btn-primary w-100">
+                        Sign Up
+                    </button>
+                </div>
+            </form>
+            
+            <div class="auth-footer">
+                Already have an account? <a href="{{ route('login.perform') }}">Sign In</a>
             </div>
         </div>
-    </section>
+    </div>
+</body>
+   
 @endsection
 
 @section('scripts')
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js"></script>
     <script>
         $(document).ready(function() {
             $('.toggle-password').click(function() {
